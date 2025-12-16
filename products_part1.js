cb61ae8f3abaa8debdbc43bed347963305a578d3
sartikaiy <indriyanisartika10@gmail.com>
2025-12-16 20:16:05 +0700

    initial upload website files

---
+++ products_part1.js
@@ -0,0 +1,122 @@
+const productsPart1 = [
+  {
+    name: "1. 💎 Gelang Titanium",
+    category: "fashion",
+    link: "https://s.shopee.co.id/BMdY4Cp4J",
+    img: "img/p01.jpg"
+  },
+  {
+    name: "2. 👜 Lanyard Dompet Jimshoney",
+    category: "fashion",
+    link: "https://s.shopee.co.id/2g456vdD3x",
+    img: "img/p02.jpg"
+  },
+  {
+    name: "3. 👟 Sepatu SOVELLA Casual",
+    category: "fashion",
+    link: "https://s.shopee.co.id/8zy25goNqT",
+    img: "img/p03.jpg"
+  },
+  {
+    name: "4. 🎨 Eyeshadow Saniye",
+    category: "beauty",
+    link: "https://s.shopee.co.id/9fDiEPNpAu",
+    img: "img/p04.jpg"
+  },
+  {
+    name: "5. 👖 ETTA Pants",
+    category: "fashion",
+    link: "https://s.shopee.co.id/20oIBcHqor",
+    img: "img/p05.jpg"
+  },
+  {
+    name: "6. 👠 Rak Sepatu Gantung",
+    category: "home",
+    link: "https://s.shopee.co.id/7V9FFoKkIP",
+    img: "img/p06.jpg"
+  },
+  {
+    name: "7. 🌂 Payung Lipat",
+    category: "home",
+    link: "https://s.shopee.co.id/3qFwt7oogN",
+    img: "img/p07.jpg"
+  },
+  {
+    name: "8. 🧥 Jaket Parka Wanita",
+    category: "fashion",
+    link: "https://s.shopee.co.id/2B7iuKJ9W2",
+    img: "img/p08.jpg"
+  },
+  {
+    name: "9. ☕ Vacuum Classic Mug",
+    category: "home",
+    link: "https://shopee.co.id/20OZ-600ml-Vacuum-Classic-Mug-Hot-and-Cold-Summer-Stainless-Steel-Tumbler-Botol-Minum-Stainless-Steel-MJG8051-i.1017875606.27962491555",
+    img: "img/p09.jpg"
+  },
+  {
+    name: "10. 🔧 Bor Listrik",
+    category: "tools",
+    link: "https://shopee.co.id/Super-Alat-Bor-Baterai-Bor-Listrik-Bor-Tanpa-kabel-399V-Set-Lengkap-Mata-Bor-Cordless-Drill-Full-Set-Of-Drill-Bits-Mesin-Bor-Tangan-Listrik-i.1291922221.28762223544",
+    img: "img/p10.jpg"
+  },
+  {
+    name: "11. 💗 Krim Kelly",
+    category: "beauty",
+    link: "https://shopee.co.id/Kelly-Pearl-Cream-15gr-5gr-Krim-Wajah-Foundation-Merah-Jingga-Official-i.1346168602.28669034511",
+    img: "img/p11.jpg"
+  },
+  {
+    name: "12. 👚 Kaos Oversize 7/8",
+    category: "fashion",
+    link: "https://shopee.co.id/KAOS-POLOS-OVERSIZE-WANITA-SLIT-SAMPING-LENGAN-7-8-i.377082421.18985036447",
+    img: "img/p12.jpg"
+  },
+  {
+    name: "13. 👕 Kemeja Rayon Salur",
+    category: "fashion",
+    link: "https://shopee.co.id/KEMEJA-WANITA-RAYON-MOTIF-SALUR-DAILY-TOP-RAYON-i.434144637.23454190903",
+    img: "img/p13.jpg"
+  },
+  {
+    name: "14. 🧵 Batik Kerja Wanita",
+    category: "fashion",
+    link: "https://shopee.co.id/Batik-Wanita-Modern-Kerja-Lengan-Panjang-Motif-Davina-Model-Terbaru-2025-i.969077646.41958535701",
+    img: "img/p14.jpg"
+  },
+  {
+    name: "15. 👖 Celana Jeans Highwaist",
+    category: "fashion",
+    link: "https://shopee.co.id/HW-Highwaist-Celana-Panjang-Jeans-Cutbray-Wanita-Stretch-Jeans-Denim-Korean-Style-Flared-Viral-Kekinian-i.1391917949.28315897285",
+    img: "img/p15.jpg"
+  },
+  {
+    name: "16. 👜 Coraldaisy Tas Bahu",
+    category: "fashion",
+    link: "https://shopee.co.id/CORALDAISY-Tas-Wanita-2025-Tali-Panjang-Tas-Selempang-Cantik-Sling-Bag-Shoulder-Bag-Elegan-Tas-Bahu-i.1384531049.40902727413",
+    img: "img/p16.jpg"
+  },
+  {
+    name: "17. 👔 CHICMORE Kemeja Oversize",
+    category: "fashion",
+    link: "https://shopee.co.id/CHICMORE-Atasan-kemeja-panjang-menengah-mewah-ringan-kelas-atas-gaya-casual-i.1383054476.28590726382",
+    img: "img/p17.jpg"
+  },
+  {
+    name: "18. 🧃 EATKIT Botol Minum",
+    category: "home",
+    link: "https://shopee.co.id/Eatkit-Botol-Minum-Transparan-Aesthetic-500ml-Tumbler-750ml-Olahraga-Portable-BPA-Free-Water-Bottle-i.1113470230.23861742554",
+    img: "img/p18.jpg"
+  },
+  {
+    name: "19. 🥤 EATKIT Tumbler Mug Coffee",
+    category: "home",
+    link: "https://shopee.co.id/Eatkit-Tumbler-Minum-Aesthetic-450ml-Tumbler-Aesthetic-Korea-Case-Kulit-Tahan-Panas-Gelas-Minum-Aesthetic-Dengan-Sedotan-i.1113470230.28058635223",
+    img: "img/p19.jpg"
+  },
+  {
+    name: "20. 💧 Wardah Micellar Water",
+    category: "beauty",
+    link: "https://shopee.co.id/Wardah-Micellar-Water-Series-Lightening-Acnederm-Perfect-Bright-Nature-Daily-i.8077122.25678566574",
+    img: "img/p20.jpg"
+  }
+];
---
+++ products_part2.js
@@ -0,0 +1,122 @@
+const productsPart2 = [
+  {
+    name: "21. 🩱 SHIERHUA Korset Pelangsing",
+    category: "fashion",
+    link: "https://shopee.co.id/SHIERHUA-S-7XL-35-145kg-korset-pelangsing-perut-buncit-korset-ibu-melahirkan-celana-korset-korset-pelangsing-celana-perut-i.1006578358.25506303528",
+    img: "img/p21.jpg"
+  },
+  {
+    name: "22. 🛏️ Sprei Homemade Aesthetic",
+    category: "home",
+    link: "https://shopee.co.id/SPREI-MOTIF-KOTAK-AESTETIC-UKURAN-120-160-DAN-180-SPREI-SATU-SET-UKURAN-SPREI-KOTAK-HITAM-SPREI-KING-SIZE-QUEN-SIZE-i.1110201503.25346144875",
+    img: "img/p22.jpg"
+  },
+  {
+    name: "23. 🎁 Coraldaisy Tas Gift",
+    category: "fashion",
+    link: "https://shopee.co.id/CORALDAISY-Tas-Wanita-2025-Cantik-Kulit-Tas-Selempang-Lucu-Handbag-Shoulder-Bag-Ritsleting-Sling-Bag-i.1384531049.40607627153",
+    img: "img/p23.jpg"
+  },
+  {
+    name: "24. 🧦 Kaos Kaki Korea",
+    category: "fashion",
+    link: "https://shopee.co.id/%E3%80%90YJH%E3%80%91Kaus-kaki-gaya-Korea-kaus-kaki-kasual-wanita-i.1478621203.26685527228",
+    img: "img/p24.jpg"
+  },
+  {
+    name: "25. 👖 ETTA Pants Putih",
+    category: "fashion",
+    link: "https://shopee.co.id/ETTA-PANTS-Semi-Flared-Celana-Highwaist-Wanita-Kulot-Loose-Pants-Panjang-Formal-Bahan-Kerja-Kantor-i.284172276.26324679004",
+    img: "img/p25.jpg"
+  },
+  {
+    name: "26. 👖 ETTA Pants Grey",
+    category: "fashion",
+    link: "https://shopee.co.id/ETTA-PANTS-Semi-Flared-Celana-Highwaist-Wanita-Kulot-Loose-Pants-Panjang-Formal-Bahan-Kerja-Kantor-i.284172276.26324679004",
+    img: "img/p26.jpg"
+  },
+  {
+    name: "27. 📱 INFINIX HOT 50 NFC",
+    category: "tools",
+    link: "https://shopee.co.id/INFINIX-HOT-50-NFC-RAM-6-256-GB-HOT50-RAM-8-128GB-GARANSI-RESMI-INFINIX-INDONESIA-i.216046779.26864621365",
+    img: "img/p27.jpg"
+  },
+  {
+    name: "28. 🛠️ Bor Grinder Mini USB",
+    category: "tools",
+    link: "https://shopee.co.id/50PCS-Bor-Mini-Grinder-Set-Penggiling-Mini-USB-Isi-Ulang-Alat-Rotary-Mikro-Pengukir-Pena-Penggiling-Listrik-Nirkabel-Set-Penggiling-Ukiran-Ukiran-Giok-Alat-Penggiling-i.1468995665.42651655603",
+    img: "img/p28.jpg"
+  },
+  {
+    name: "29. 🎧 Sleep Earbuds Bluetooth",
+    category: "tools",
+    link: "https://shopee.co.id/X55-Sleep-Earbuds-Bluetooth-Wireless-Headphones-Earbuds-Ear-Hooks-Bluetooth-Earphones-Bass-Noise-Reduction-Earbuds-HiFi-Stereo-Headsets-i.263328239.41857172424",
+    img: "img/p29.jpg"
+  },
+  {
+    name: "30. 💅 KUDAN Kutek",
+    category: "beauty",
+    link: "https://shopee.co.id/KUDAN-40-WARNA-KUTEK-Halal-kutek-Peel-Off-kutek-Muslimah-Bisa-Dikupas-Bisa-Untuk-Sholat-Kuteks-10ML-i.361976631.25817863880",
+    img: "img/p30.jpg"
+  },
+  {
+    name: "31. 🪟 Tirai Jendela Aesthetic",
+    category: "home",
+    link: "https://shopee.co.id/60X180-cm-Aluminium-TIRAI-GULUNG-LIPAT-JENDELA-60X180-cm-Venetian-Blinds-25-mm-0.18mm-Tirai-Penutup-Kerai-Hordeng-Penghalau-Cahaya-Aksesoris-Jendela-Kemasan-film-gelembung-i.1028963329.25162451572",
+    img: "img/p31.jpg"
+  },
+  {
+    name: "32. 👜 Tas Shoulder Bag Premium",
+    category: "fashion",
+    link: "https://shopee.co.id/TAS-SHOULDER-BAG-ZEIN-BENDUL-PREMIUM-i.927131991.21075341060",
+    img: "img/p32.jpg"
+  },
+  {
+    name: "33. 👖 ETTA Pants Hitam",
+    category: "fashion",
+    link: "https://shopee.co.id/ETTA-PANTS-%E2%80%93-Celana-Flare-Wanita-Highwaist-Kekinian-%E2%80%93-Kulot-Cutbray-Celana-Kerja-Bahan-Anti-Kusut-i.780902686.28230537573",
+    img: "img/p33.jpg"
+  },
+  {
+    name: "34. 🛏️ Sprei Homemade Flora",
+    category: "home",
+    link: "https://shopee.co.id/Sprei-Homemade-flora-120-160-180-i.707266774.17375235233",
+    img: "img/p34.jpg"
+  },
+  {
+    name: "35. 🎧 Headset Wireless Aesthetic",
+    category: "tools",
+    link: "https://shopee.co.id/(FREE-GIFT-)SKYMICOOL-Wireless-Headphone-Bluetooth-5.3-Hifi-Stereo-Esport-Gaming-Headset-Noise-Reduction-TH10-HS500-i.1501772304.26783516647",
+    img: "img/p35.jpg"
+  },
+  {
+    name: "36. 🧱 Karpet Lantai Vinyl",
+    category: "home",
+    link: "https://shopee.co.id/Wallpaper-Home-Karpet-Lantai-Vinyl-Meteran-Lebar-2-Meter-Panjang-50-Cm-Tebal-0.7-Mm-Anti-Slip-i.1541818434.43150980397",
+    img: "img/p36.jpg"
+  },
+  {
+    name: "37. 👚 Jean Blouse Jumbo Wanita",
+    category: "fashion",
+    link: "https://shopee.co.id/JEAN-BLOUSE-Atasan-Wanita-Casual-Terlaris-Blouse-Vintage-i.33239640.23919989507",
+    img: "img/p37.jpg"
+  },
+  {
+    name: "38. 🌂 Payung Lipat Otomatis",
+    category: "home",
+    link: "https://shopee.co.id/Satu-Keluarga-Payung-Lipat-Otomatis-Buka-Tutup-Lipat-Anti-UV-C432-Payung-UV-Nivil-Perlindungan-Matahari-Payung-Hujan-Anti-Sinar-Matahari-UV-i.494896079.17189627222",
+    img: "img/p38.jpg"
+  },
+  {
+    name: "39. 🛏️ INTHEBOX Alpha X",
+    category: "home",
+    link: "https://shopee.co.id/INTHEBOX-ALPHA-X-Kasur-Springbed-FREE-Bantal-Ukuran-kasur-90x200-100x200-120x200-140x200-160x200-180x200-200x200-i.40076058.15933374243",
+    img: "img/p39.jpg"
+  },
+  {
+    name: "40. 📱 Xiaomi Poco M6",
+    category: "tools",
+    link: "https://shopee.co.id/Xiaomi-Poco-M6-8GB-256GB-Garansi-Resmi-1-Tahun-i.59822858.28559653746",
+    img: "img/p40.jpg"
+  }
+];
